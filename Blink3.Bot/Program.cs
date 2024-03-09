d22fd332fa5ee9b72c0e32d44d193f5092576f51
EpicOfficer <josh@collis.email>
2024-03-09 10:51:14 +0000

    Add Discord.Net dependencies and implement basic bot setup
    Two new package references, Discord.Addons.Hosting and Discord.Net, have been added to the project file. Moreover, an initial setup for a Discord bot has been implemented in the Program.cs file, replacing the previous NotImplementedException. This includes setting up a Host Builder, configuring the Discord bot, and adding an interaction service.

--- Blink3.Bot/Program.cs
+++ Blink3.Bot/Program.cs
@@ -1,1 +1,29 @@
-﻿throw new NotImplementedException();
+﻿using Discord;
+using Discord.Addons.Hosting;
+using Discord.WebSocket;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddDiscordShardedHost((config, _) =>
+{
+    config.SocketConfig = new DiscordSocketConfig
+    {
+        LogLevel = LogSeverity.Verbose,
+        AlwaysDownloadUsers = true,
+        MessageCacheSize = 200,
+        GatewayIntents = GatewayIntents.AllUnprivileged
+    };
+    
+    config.Token = builder.Configuration["Token"]!;
+});
+
+builder.Services.AddInteractionService((config, _) =>
+{
+    config.LogLevel = LogSeverity.Info;
+    config.UseCompiledLambda = true;
+});
+
+var host = builder.Build();
+
+await host.RunAsync();
