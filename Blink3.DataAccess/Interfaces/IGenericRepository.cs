b120e9d45beb275ea3d945e296cf487769424b51
EpicOfficer <josh@collis.email>
2024-03-10 14:11:35 +0000

    Remove IReadOnlyRepository interface
    IReadOnlyRepository interface has been deleted and its methods were moved to IGenericRepository. This change simplifies the code structure by reducing the number of interfaces and improving their cohesiveness.

--- Blink3.DataAccess/Interfaces/IGenericRepository.cs
+++ Blink3.DataAccess/Interfaces/IGenericRepository.cs
@@ -1,7 +1,9 @@
 namespace Blink3.DataAccess.Interfaces;
 
-public interface IGenericRepository<T> : IReadOnlyRepository<T> where T : class
+public interface IGenericRepository<T> where T : class
 {
+    Task<T?> GetByIdAsync(params object[] keyValues);
+    Task<IReadOnlyCollection<T>> GetAllAsync();
     Task<T> AddAsync(T entity);
     Task<T> UpdateAsync(T entity);
     Task DeleteAsync(T entity);
--- Blink3.DataAccess/Interfaces/IReadOnlyRepository.cs
+++
@@ -1,7 +0,0 @@
-namespace Blink3.DataAccess.Interfaces;
-
-public interface IReadOnlyRepository<T> where T : class
-{
-    Task<T?> GetByIdAsync(params object[] keyValues);
-    Task<IReadOnlyCollection<T>> GetAllAsync();
-}
