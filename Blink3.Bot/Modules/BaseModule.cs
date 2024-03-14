99e43b380e08314d5d36052f1fc8dc35740cf9c8
EpicOfficer <josh@collis.email>
2024-03-14 15:31:54 +0000

    Remove Blink3.Bot.Tests and expand TodoModule
    Blink3.Bot.Tests project has been removed from the solution as it is no longer needed. In addition, the TodoModule was expanded, adding the 'view' command for viewing the user's todo list. Finally, a new method to respond with a success message was added to the BaseModule.

--- Blink3.Bot/Modules/BaseModule.cs
+++ Blink3.Bot/Modules/BaseModule.cs
@@ -5,4 +5,8 @@ namespace Blink3.Bot.Modules;
 
 public class BlinkModuleBase<T> : InteractionModuleBase<T> where T : class, IInteractionContext
 {
+    protected async Task RespondSuccessAsync(string text)
+    {
+        await RespondAsync(text);
+    }
 }
--- Blink3.Bot/Modules/TodoModule.cs
+++ Blink3.Bot/Modules/TodoModule.cs
@@ -1,8 +1,39 @@
+using System.Text;
+using Blink3.DataAccess.Entities;
+using Blink3.DataAccess.Interfaces;
 using Discord.Interactions;
 
 namespace Blink3.Bot.Modules;
 
-public class TodoModule : BlinkModuleBase<SocketInteractionContext>
+[Group("todo", "Simple todo list")]
+public class TodoModule(IUserTodoRepository todoRepository) : BlinkModuleBase<SocketInteractionContext>
 {
-    
+    [SlashCommand("view", "View your todo list")]
+    public async Task View()
+    {
+        IReadOnlyCollection<UserTodo> todos = await todoRepository.GetByUserIdAsync(Context.User.Id!);
+        if (todos.Count < 1)
+        {
+            await RespondSuccessAsync("You don't have any todo items!");
+            return;
+        }
+
+        StringBuilder sb = new();
+        sb.AppendLine("**Your todo list**");
+        
+        foreach (UserTodo todo in todos)
+        {
+            if (todo.Complete) sb.Append("~~");
+            
+            sb.AppendLine($"**{todo.Label}**");
+            if (!string.IsNullOrWhiteSpace(todo.Description))
+                sb.AppendLine(todo.Description);
+
+            if (todo.Complete) sb.Append("~~");
+            
+            sb.AppendLine();
+        }
+
+        await RespondSuccessAsync(sb.ToString());
+    }
 }
