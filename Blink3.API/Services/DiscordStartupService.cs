59b9a0028e28fff1b0fc8451587e891228385424
EpicOfficer <josh@collis.email>
2024-03-15 22:52:31 +0000

    Update DiscordStartupService configuration
    Added SuppressMessage attribute to DiscordStartupService constructor to control ReSharper warning. Replaced generic IConfiguration with BlinkConfiguration to leverage specific settings. Also, the Discord bot token access has been updated for better efficiency and clarity.

--- Blink3.API/Services/DiscordStartupService.cs
+++ Blink3.API/Services/DiscordStartupService.cs
@@ -1,13 +1,16 @@
+using System.Diagnostics.CodeAnalysis;
+using Blink3.Common.Configuration;
 using Discord;
 using Discord.Rest;
 
 namespace Blink3.API.Services;
 
-public class DiscordStartupService(DiscordRestClient client, IConfiguration config, ILogger<DiscordStartupService> logger) : IHostedService
+[SuppressMessage("ReSharper", "SuggestBaseTypeForParameterInConstructor")]
+public class DiscordStartupService(DiscordRestClient client, BlinkConfiguration config, ILogger<DiscordStartupService> logger) : IHostedService
 {
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        await client.LoginAsync(TokenType.Bot, config["Token"]!);
+        await client.LoginAsync(TokenType.Bot, config.Discord.BotToken);
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
