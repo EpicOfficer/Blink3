7d516513360cdcdda87772b1c7030fbe3286f833
EpicOfficer <josh@collis.email>
2024-03-18 18:08:21 +0000

    Add Serilog logging to application
    Serilog logging infrastructure has been incorporated into the WebApplication creation flow. The logging setup includes writing to the console. Also established structure to catch, log and handle exceptions during application start-up. Updated the Blink3.API.csproj file by adding the necessary Serilog packages.

--- Blink3.API/Program.cs
+++ Blink3.API/Program.cs
@@ -8,69 +8,90 @@ using Blink3.DataAccess.Extensions;
 using Discord.Rest;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Serilog;
+using Serilog.Events;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+    .WriteTo.Console()
+    .CreateLogger();
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Add Application configurations
-builder.Services.AddAppConfiguration(builder.Configuration);
-BlinkConfiguration appConfig = builder.Services.GetAppConfiguration();
-
-// Add Data Access layer and cache provider
-builder.Services.AddDataAccess(appConfig);
-builder.Services.AddCaching(appConfig);
-
-// Add Discord Rest Client and startup service
-builder.Services.AddSingleton<DiscordRestClient>();
-builder.Services.AddHostedService<DiscordStartupService>();
-
-// Configure Authentication and Discord OAuth
-builder.Services.AddAuthentication(options =>
+try
 {
-    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = DiscordAuthenticationDefaults.AuthenticationScheme;
-})
-.AddCookie()
-.AddOAuth(DiscordAuthenticationDefaults.AuthenticationScheme, options =>
-{
-    options.ClientId = appConfig.Discord.ClientId;
-    options.ClientSecret = appConfig.Discord.ClientSecret;
-    options.CallbackPath = new PathString("/api/auth/callback");
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-    options.AuthorizationEndpoint = DiscordAuthenticationDefaults.AuthorizationEndpoint;
-    options.TokenEndpoint = DiscordAuthenticationDefaults.TokenEndpoint;
-    options.UserInformationEndpoint = DiscordAuthenticationDefaults.UserInformationEndpoint;
-    
-    options.Scope.Add("identify");
-    options.Scope.Add("guilds");
-    
-    options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
-    options.ClaimActions.MapJsonKey(ClaimTypes.Name, "username");
-    options.ClaimActions.MapJsonKey(ClaimTypes.GivenName, "global_name");
-    
-    options.SaveTokens = true;
-});
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Host.UseSerilog();
 
-WebApplication app = builder.Build();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Add Application configurations
+    builder.Services.AddAppConfiguration(builder.Configuration);
+    BlinkConfiguration appConfig = builder.Services.GetAppConfiguration();
+
+    // Add Data Access layer and cache provider
+    builder.Services.AddDataAccess(appConfig);
+    builder.Services.AddCaching(appConfig);
+
+    // Add Discord Rest Client and startup service
+    builder.Services.AddSingleton<DiscordRestClient>();
+    builder.Services.AddHostedService<DiscordStartupService>();
+
+    // Configure Authentication and Discord OAuth
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = DiscordAuthenticationDefaults.AuthenticationScheme;
+    })
+    .AddCookie()
+    .AddOAuth(DiscordAuthenticationDefaults.AuthenticationScheme, options =>
+    {
+        options.ClientId = appConfig.Discord.ClientId;
+        options.ClientSecret = appConfig.Discord.ClientSecret;
+        options.CallbackPath = new PathString("/api/auth/callback");
+
+        options.AuthorizationEndpoint = DiscordAuthenticationDefaults.AuthorizationEndpoint;
+        options.TokenEndpoint = DiscordAuthenticationDefaults.TokenEndpoint;
+        options.UserInformationEndpoint = DiscordAuthenticationDefaults.UserInformationEndpoint;
+        
+        options.Scope.Add("identify");
+        options.Scope.Add("guilds");
+        
+        options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+        options.ClaimActions.MapJsonKey(ClaimTypes.Name, "username");
+        options.ClaimActions.MapJsonKey(ClaimTypes.GivenName, "global_name");
+        
+        options.SaveTokens = true;
+    });
+
+    WebApplication app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+    
+    app.MapControllers();
+
+    app.Run(); 
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run(); 
+catch (Exception e)
+{
+    Log.Fatal(e, "Application start-up failed.");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
