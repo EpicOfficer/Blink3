2d72631ff60637df3bb0864a84139cc9cc25eb2c
EpicOfficer <josh@collis.email>
2024-03-18 20:46:32 +0000

    Refactor OAuth options configuration in ServiceCollectionExtensions
    This commit simplifies the code for configuring OAuth options within the ServiceCollectionExtensions class by moving the logic into a separate method. It also separates the task of fetching user data from Discord and creating corresponding claims into its own method, improving readability and maintainability.

--- Blink3.API/Extensions/ServiceCollectionExtensions.cs
+++ Blink3.API/Extensions/ServiceCollectionExtensions.cs
@@ -5,8 +5,8 @@ using AspNet.Security.OAuth.Discord;
 using Blink3.API.Models;
 using Blink3.Common.Configuration;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Serilog;
-
 namespace Blink3.API.Extensions;
 
 public static class ServiceCollectionExtensions
@@ -14,61 +14,65 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddDiscordAuth(this IServiceCollection services, BlinkConfiguration appConfig)
     {
         services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = DiscordAuthenticationDefaults.AuthenticationScheme;
-            })
-            .AddCookie()
-            .AddOAuth(DiscordAuthenticationDefaults.AuthenticationScheme, options =>
-            {
-                options.ClientId = appConfig.Discord.ClientId;
-                options.ClientSecret = appConfig.Discord.ClientSecret;
-                options.CallbackPath = new PathString("/api/auth/callback");
+        {
+            options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = DiscordAuthenticationDefaults.AuthenticationScheme;
+        })
+        .AddCookie()
+        .AddOAuth(DiscordAuthenticationDefaults.AuthenticationScheme, options => ConfigureOAuthOptions(options, appConfig));
 
-                options.AuthorizationEndpoint = DiscordAuthenticationDefaults.AuthorizationEndpoint;
-                options.TokenEndpoint = DiscordAuthenticationDefaults.TokenEndpoint;
-                options.UserInformationEndpoint = DiscordAuthenticationDefaults.UserInformationEndpoint;
-                
-                options.Scope.Add("identify");
-                options.Scope.Add("guilds");
-                
-                options.Events.OnCreatingTicket = async context =>
-                {
-                    // Make a GET request to UserInformationEndpoint, include the access token in Authorization header
-                    HttpRequestMessage requestMessage = new(HttpMethod.Get, options.UserInformationEndpoint);
-                    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-                    HttpResponseMessage backchannelResponse = await options.Backchannel.SendAsync(requestMessage, context.HttpContext.RequestAborted);
-
-                    if (backchannelResponse.IsSuccessStatusCode)
-                    {
-                        string userInformationJson = await backchannelResponse.Content.ReadAsStringAsync();
-                        // Parse the JSON object
-                        DiscordUserIdentity userInformation = JsonSerializer.Deserialize<DiscordUserIdentity>(userInformationJson)
-                                                              ?? throw new InvalidOperationException("Unable to parse json response from user information endpoint.");
-                        
-                        context.Identity?.AddClaim(new Claim(ClaimTypes.NameIdentifier, userInformation.Id));
-                        context.Identity?.AddClaim(new Claim(ClaimTypes.Name, userInformation.Username));
-                        
-                        if (userInformation.GlobalName is not null)
-                        {
-                            context.Identity?.AddClaim(new Claim(ClaimTypes.GivenName, userInformation.GlobalName));
-                        }
-
-                        if (userInformation.Locale is not null)
-                        {
-                            context.Identity?.AddClaim(new Claim(ClaimTypes.Locality, userInformation.Locale));
-                        }
-                    }
-                    else
-                    {
-                        Log.Warning("Could not retrieve user data for token {Token}", context.AccessToken);
-                    }
-                };
-                
-                options.SaveTokens = true;
-            });
-         
         return services;
+    }
+
+    private static void ConfigureOAuthOptions(OAuthOptions options, BlinkConfiguration appConfig) 
+    {
+        options.ClientId = appConfig.Discord.ClientId;
+        options.ClientSecret = appConfig.Discord.ClientSecret;
+        options.CallbackPath = new PathString("/api/auth/callback");
+        options.AuthorizationEndpoint = DiscordAuthenticationDefaults.AuthorizationEndpoint;
+        options.TokenEndpoint = DiscordAuthenticationDefaults.TokenEndpoint;
+        options.UserInformationEndpoint = DiscordAuthenticationDefaults.UserInformationEndpoint;
+
+        options.Scope.Add("identify");
+        options.Scope.Add("guilds");
+
+        options.Events.OnCreatingTicket = async context => 
+        {
+            await FetchDiscordUserInfoAndCreateClaims(context, options);
+        };
+
+        options.SaveTokens = true;
+    }
+
+    private static async Task FetchDiscordUserInfoAndCreateClaims(OAuthCreatingTicketContext context, OAuthOptions options) 
+    {
+        HttpRequestMessage requestMessage = new(HttpMethod.Get, options.UserInformationEndpoint);
+        requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+        HttpResponseMessage backchannelResponse = await options.Backchannel.SendAsync(requestMessage, context.HttpContext.RequestAborted);
+
+        if (backchannelResponse.IsSuccessStatusCode)
+        {
+            string userInformationJson = await backchannelResponse.Content.ReadAsStringAsync();
+            DiscordUserIdentity userInformation = JsonSerializer.Deserialize<DiscordUserIdentity>(userInformationJson)
+                                                      ?? throw new InvalidOperationException("Unable to parse json response from user information endpoint.");
+
+            context.Identity?.AddClaim(new Claim(ClaimTypes.NameIdentifier, userInformation.Id));
+            context.Identity?.AddClaim(new Claim(ClaimTypes.Name, userInformation.Username));
+
+            if (userInformation.GlobalName is not null)
+            {
+                context.Identity?.AddClaim(new Claim(ClaimTypes.GivenName, userInformation.GlobalName));
+            }
+
+            if (userInformation.Locale is not null)
+            {
+                context.Identity?.AddClaim(new Claim(ClaimTypes.Locality, userInformation.Locale));
+            }
+        }
+        else
+        {
+            Log.Warning("Could not retrieve user data for token {Token}", context.AccessToken);
+        }
     }
 }
