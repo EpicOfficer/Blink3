ed1eaf31aaa2e884a12359d2f0afd0a95b0202e6
EpicOfficer <josh@collis.email>
2024-03-22 14:15:43 +0000

    Refactor Todo component and remove unused extension
    The Todo page in the Blink3.Web project has been refactored. "Create Todo" form previously within the same page has been moved to a separate component called CreateTodoForm for better componentization and modularity. Furthermore, an unused service extension in the ServiceCollectionExtensions.cs in the Blink3.Common project has been removed, reducing unnecessary clutter.

--- Blink3.Common/Configuration/Extensions/ServiceCollectionExtensions.cs
+++ Blink3.Common/Configuration/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,5 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
 
 namespace Blink3.Common.Configuration.Extensions;
