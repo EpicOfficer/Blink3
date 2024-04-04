0618002b6c1afd44e5729cca335e58521656fac8
EpicOfficer <josh@collis.email>
2024-04-04 20:30:50 +0100

    Integrate AuthContext in App component
    The AuthContext has been imported and utilized within the App component. Conditions have been added to handle cases where the AuthContext has not yet been initialized or when the user is not yet authenticated, with respective loading and logging-in messages being displayed.

--- Blink3.Activity/src/App.tsx
+++ Blink3.Activity/src/App.tsx
@@ -1,6 +1,18 @@
 // noinspection JSIgnoredPromiseFromCall
+import {AuthContext} from "./contexts/AuthContext.tsx";
+import {useContext} from "react";
 
 export default function App() {
+    const authContext = useContext(AuthContext);
+    
+    if (!authContext) {
+        return <p>Loading...</p>;
+    }
+
+    if (!authContext.authenticated) {
+        return <p>Logging in...</p>;
+    }
+    
     return (
         <p>Hello world</p>
     )
