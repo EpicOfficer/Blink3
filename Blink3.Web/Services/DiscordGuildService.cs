d0b98ace563ecfbcae95f0cd104a4c39c4226333
EpicOfficer <9379778+EpicOfficer@users.noreply.github.com>
2024-04-19 14:22:49 +0100

    Update Staff.razor and fix method names in DiscordGuildService
    The Staff.razor page now supports viewing, selecting, and editing of staff settings. This includes loading channel options, displaying an informative message, and saving changes made to the settings. Meanwhile, corrected method names in the DiscordGuildService, making them appropriately relay their function.

--- Blink3.Web/Services/DiscordGuildService.cs
+++ Blink3.Web/Services/DiscordGuildService.cs
@@ -6,14 +6,14 @@ namespace Blink3.Web.Services;
 
 public class DiscordGuildService(HttpClient httpClient) : IDiscordGuildService
 {
-    public async Task<IEnumerable<DiscordPartialChannel>> GetChannels(string? guildId)
+    public async Task<IEnumerable<DiscordPartialChannel>> GetCategories(string? guildId)
     {
         if (string.IsNullOrWhiteSpace(guildId)) return [];
         return await httpClient.GetFromJsonAsync<IEnumerable<DiscordPartialChannel>>(
             $"/api/Guilds/{guildId}/categories") ?? [];
     }
 
-    public async Task<IEnumerable<DiscordPartialChannel>> GetCategories(string? guildId)
+    public async Task<IEnumerable<DiscordPartialChannel>> GetChannels(string? guildId)
     {
         if (string.IsNullOrWhiteSpace(guildId)) return [];
         return await httpClient.GetFromJsonAsync<IEnumerable<DiscordPartialChannel>>(
