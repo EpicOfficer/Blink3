1c9e1112be014b37c56ec240f45e94ed84f64ec3
EpicOfficer <9379778+EpicOfficer@users.noreply.github.com>
2025-07-03 17:40:12 +0100

    Add detailed logging to TodoModule
    - Integrated `ILogger` to track key actions in todo commands.
    - Added `UserLogContext` for enhanced user activity traceability.
    - Enhanced error handling logs for improved debugging and monitoring.

--- Blink3.Bot/Modules/TodoModule.cs
+++ Blink3.Bot/Modules/TodoModule.cs
@@ -1,13 +1,14 @@
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
-using System.Numerics;
 using Blink3.Bot.Extensions;
 using Blink3.Bot.MessageStyles;
 using Blink3.Bot.Modals;
 using Blink3.Core.Entities;
 using Blink3.Core.Interfaces;
+using Blink3.Core.LogContexts;
 using Discord;
 using Discord.Interactions;
+using Microsoft.Extensions.Logging;
 
 // ReSharper disable UnusedType.Global
 // ReSharper disable UnusedMember.Global
@@ -17,7 +18,7 @@ namespace Blink3.Bot.Modules;
 [CommandContextType(InteractionContextType.Guild, InteractionContextType.BotDm, InteractionContextType.PrivateChannel)]
 [IntegrationType(ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall)]
 [Group("todo", "Simple todo list")]
-public class TodoModule(IUnitOfWork unitOfWork) : BlinkModuleBase<IInteractionContext>(unitOfWork)
+public class TodoModule(IUnitOfWork unitOfWork, ILogger<TodoModule> logger) : BlinkModuleBase<IInteractionContext>(unitOfWork)
 {
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
@@ -42,24 +43,30 @@ public class TodoModule(IUnitOfWork unitOfWork) : BlinkModuleBase<IInteractionCo
         [MaxLength(50)] [Description("A longer description for the todo list item.  Max 50 characters")]
         string? description = null)
     {
-        int count = await _unitOfWork.UserTodoRepository.GetCountByUserIdAsync(Context.User.Id);
-        if (count >= 25)
+        UserLogContext userLogContext = new(Context.User);
+
+        using (logger.BeginScope(new { User = userLogContext }))
         {
-            await RespondErrorAsync("Too many todo items!",
-                "You cannot create more than 25 todo list items, please remove some first!");
-            return;
+            int count = await _unitOfWork.UserTodoRepository.GetCountByUserIdAsync(Context.User.Id);
+            if (count >= 25)
+            {
+                await RespondErrorAsync("Too many todo items!",
+                    "You cannot create more than 25 todo list items, please remove some first!");
+                return;
+            }
+
+            await _unitOfWork.UserTodoRepository.AddAsync(new UserTodo
+            {
+                UserId = Context.User.Id,
+                Label = label,
+                Description = description,
+                Complete = false
+            });
+            await _unitOfWork.SaveChangesAsync();
+
+            await RespondSuccessAsync($"Created todo item \"{label}\"");
+            logger.LogInformation("{User} Created a todo list item", userLogContext);
         }
-
-        await _unitOfWork.UserTodoRepository.AddAsync(new UserTodo
-        {
-            UserId = Context.User.Id,
-            Label = label,
-            Description = description,
-            Complete = false
-        });
-        await _unitOfWork.SaveChangesAsync();
-
-        await RespondSuccessAsync($"Created todo item \"{label}\"");
     }
 
     [SlashCommand("view", "View or edit your todo list")]
@@ -67,87 +74,109 @@ public class TodoModule(IUnitOfWork unitOfWork) : BlinkModuleBase<IInteractionCo
         [Description("Whether to post the todo list so that it is visible to other users.")]
         bool postPublicly = false)
     {
-        IGuildUser? user = Context.User as IGuildUser;
-        IReadOnlyCollection<UserTodo> todos = await _unitOfWork.UserTodoRepository.GetByUserIdAsync(Context.User.Id);
+        UserLogContext userLogContext = new(Context.User);
 
-        ContainerBuilder builder = new ContainerBuilder()
-            .WithAccentColor(Colours.Info)
-            .WithSection(new SectionBuilder()
-                .WithTextDisplay($"## {(user is null ? "Your" : user.GetFriendlyName() + "'s")} todo list.")
-                .WithAccessory(new ButtonBuilder("Add", "todo:addButton")))
-            .WithSeparator(isDivider: false);
-
-        if (todos.Count == 0)
+        using (logger.BeginScope(new { User = userLogContext }))
         {
-            builder.WithTextDisplay("🔍 Your todo list is currently empty. Add a new item using the **Add Item** button above!");
-        }
+            IGuildUser? user = Context.User as IGuildUser;
+            IReadOnlyCollection<UserTodo> todos = await _unitOfWork.UserTodoRepository.GetByUserIdAsync(Context.User.Id);
 
-        foreach (UserTodo todo in todos)
-        {
-            string completionIcon = todo.Complete ? Icons.BoxChecked : Icons.Box;
-            ButtonStyle style = todo.Complete ? ButtonStyle.Secondary : ButtonStyle.Success;
+            ContainerBuilder builder = new ContainerBuilder()
+                .WithAccentColor(Colours.Info)
+                .WithSection(new SectionBuilder()
+                    .WithTextDisplay($"## {(user is null ? "Your" : user.GetFriendlyName() + "'s")} todo list.")
+                    .WithAccessory(new ButtonBuilder("Add", "todo:addButton")))
+                .WithSeparator(isDivider: false);
+
+            if (todos.Count == 0)
+            {
+                builder.WithTextDisplay("🔍 Your todo list is currently empty. Add a new item using the **Add Item** button above!");
+            }
+
+            foreach (UserTodo todo in todos)
+            {
+                string completionIcon = todo.Complete ? Icons.BoxChecked : Icons.Box;
+                ButtonStyle style = todo.Complete ? ButtonStyle.Secondary : ButtonStyle.Success;
             
-            builder.WithSeparator()
-                .WithTextDisplay($"""
-                                        ### {completionIcon} {todo.Label}
-                                        {todo.Description ?? "_ _"}
-                                        """)
-                .WithSeparator(isDivider: false, spacing: SeparatorSpacingSize.Small)
-                .WithActionRow(new ActionRowBuilder()
-                    .WithButton($"{(todo.Complete ? "Mark Incomplete" : "Mark Complete")}", 
-                        $"todo:toggleComplete_{todo.Id}",
-                        style)
-                    .WithButton("Remove", $"todo:remove_{todo.Id}", ButtonStyle.Danger));
-        }
+                builder.WithSeparator()
+                    .WithTextDisplay($"""
+                                      ### {completionIcon} {todo.Label}
+                                      {todo.Description ?? "_ _"}
+                                      """)
+                    .WithSeparator(isDivider: false, spacing: SeparatorSpacingSize.Small)
+                    .WithActionRow(new ActionRowBuilder()
+                        .WithButton($"{(todo.Complete ? "Mark Incomplete" : "Mark Complete")}", 
+                            $"todo:toggleComplete_{todo.Id}",
+                            style)
+                        .WithButton("Remove", $"todo:remove_{todo.Id}", ButtonStyle.Danger));
+            }
 
-        ComponentBuilderV2 components = new(builder);
-        await RespondOrFollowUpAsync(components: components.Build(), ephemeral: !postPublicly);
+            ComponentBuilderV2 components = new(builder);
+            await RespondOrFollowUpAsync(components: components.Build(), ephemeral: !postPublicly);
+            logger.LogInformation("{User} Viewed their todo list", userLogContext);
+        }
     }
 
     [ComponentInteraction("todo:toggleComplete_*", true)]
     public async Task ToggleComplete(int id)
     {
-        UserTodo? todo = await _unitOfWork.UserTodoRepository.GetByIdAsync(id);
-        if (todo is null)
+        UserLogContext userLogContext = new(Context.User);
+
+        using (logger.BeginScope(new { User = userLogContext }))
         {
-            await RespondErrorAsync("Todo not found!", "I could not find that item on your todo list...");
-            return;
-        }
+            UserTodo? todo = await _unitOfWork.UserTodoRepository.GetByIdAsync(id);
+            if (todo is null)
+            {
+                logger.LogInformation("{User} Tried to modify a todo list item that does not exist.", userLogContext);
+                await RespondErrorAsync("Todo not found!", "I could not find that item on your todo list...");
+                return;
+            }
         
-        if (Context.User.Id != todo.UserId)
-        {
-            await RespondErrorAsync("You cannot edit this item!", "Only the person who created this item can edit it.");
-            return;
+            if (Context.User.Id != todo.UserId)
+            {
+                logger.LogInformation("{User} Tried to modify a todo list item that does not belong to them.", userLogContext);
+                await RespondErrorAsync("You cannot edit this item!", "Only the person who created this item can edit it.");
+                return;
+            }
+
+            todo.Complete = !todo.Complete;
+
+            await _unitOfWork.UserTodoRepository.UpdateAsync(todo);
+            await _unitOfWork.SaveChangesAsync();
+
+            await RespondSuccessAsync($"Marked todo as {(todo.Complete ? "complete" : "incomplete")}");
+            logger.LogInformation("{User} Updated their todo list.", userLogContext);
         }
-
-        todo.Complete = !todo.Complete;
-
-        await _unitOfWork.UserTodoRepository.UpdateAsync(todo);
-        await _unitOfWork.SaveChangesAsync();
-
-        await RespondSuccessAsync($"Marked todo as {(todo.Complete ? "complete" : "incomplete")}");
     }
 
     [ComponentInteraction("todo:remove_*", true)]
     public async Task Remove(int id)
     {
-        UserTodo? todo = await _unitOfWork.UserTodoRepository.GetByIdAsync(id);
-        
-        if (todo is null)
-        {
-            await RespondErrorAsync("Todo not found!", "I could not find that item on your todo list...");
-            return;
-        }
-        
-        if (Context.User.Id != todo.UserId)
-        {
-            await RespondErrorAsync("You cannot remove this item!", "Only the person who created this item can remove it.");
-            return;
-        }
-        
-        await _unitOfWork.UserTodoRepository.DeleteAsync(todo);
-        await _unitOfWork.SaveChangesAsync();
+        UserLogContext userLogContext = new(Context.User);
 
-        await RespondSuccessAsync("Todo item removed");
+        using (logger.BeginScope(new { User = userLogContext }))
+        {
+            UserTodo? todo = await _unitOfWork.UserTodoRepository.GetByIdAsync(id);
+        
+            if (todo is null)
+            {
+                logger.LogInformation("{User} Tried to remove a todo list item that does not exist.", userLogContext);
+                await RespondErrorAsync("Todo not found!", "I could not find that item on your todo list...");
+                return;
+            }
+        
+            if (Context.User.Id != todo.UserId)
+            {
+                logger.LogInformation("{User} Tried to remove an item from somebody else's todo list.", userLogContext);
+                await RespondErrorAsync("You cannot remove this item!", "Only the person who created this item can remove it.");
+                return;
+            }
+        
+            await _unitOfWork.UserTodoRepository.DeleteAsync(todo);
+            await _unitOfWork.SaveChangesAsync();
+
+            await RespondSuccessAsync("Todo item removed");
+            logger.LogInformation("{User} Removed an item from their todo list.", userLogContext);
+        }
     }
 }
