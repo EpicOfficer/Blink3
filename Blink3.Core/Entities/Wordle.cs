f4a649388b8fdf92b62f7546291a99163048876b
EpicOfficer <9379778+EpicOfficer@users.noreply.github.com>
2025-07-22 10:49:38 +0100

    Make `Language` and `WordToGuess` properties immutable in Wordle entity.

--- Blink3.Core/Entities/Wordle.cs
+++ Blink3.Core/Entities/Wordle.cs
@@ -15,25 +15,37 @@ public class Wordle : GameBase
 {
     public override GameType Type => GameType.BlinkWord;
     
+    private readonly string _language = string.Empty;
+    private readonly string _wordToGuess = string.Empty;
+
+    
     /// <summary>
-    ///     The language this wordle was started in
+    ///     The language this Wordle was started in.
     /// </summary>
-    public string Language { get; set; } = string.Empty;
+    public string Language
+    {
+        get => _language;
+        init => _language = value ?? throw new ArgumentNullException(nameof(value));
+    }
 
     /// <summary>
     ///     Represents the word to guess in the Wordle game.
     /// </summary>
     [Required]
     [MaxLength(8)]
-    public string WordToGuess { get; set; } = string.Empty;
-
-    /// <summary>
-    ///     The total number of guesses in this wordle game.
-    /// </summary>
-    public int TotalAttempts => Guesses.Count;
+    public string WordToGuess
+    {
+        get => _wordToGuess;
+        init => _wordToGuess = value ?? throw new ArgumentNullException(nameof(value));
+    }
 
     /// <summary>
     ///     Represents the collection of guesses made by players in a Wordle game.
     /// </summary>
     public ICollection<WordleGuess> Guesses { get; set; } = [];
+    
+    /// <summary>
+    ///     The total number of guesses in this wordle game.
+    /// </summary>
+    public int TotalAttempts => Guesses.Count;
 }
